513dd3b04e33599b2affb9ba4ef5161b6c2bc599
Pierre Houedry <72455069+pierrehouedry@users.noreply.github.com>
2024-11-22 11:47:31 +0100

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -246,7 +246,12 @@ const GitProfile = ({ config }: { config: Config }) => {
                 </div>
                 <div className="lg:col-span-2 col-span-1">
                   <div className="grid grid-cols-1 gap-6">
-                   
+                    {sanitizedConfig.publications.length !== 0 && (
+                      <PublicationCard
+                        loading={loading}
+                        publications={sanitizedConfig.publications}
+                      />
+                    )}
                     {sanitizedConfig.projects.github.display && (
                       <GithubProjectCard
                         header={sanitizedConfig.projects.github.header}
@@ -257,12 +262,7 @@ const GitProfile = ({ config }: { config: Config }) => {
                         googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
                       />
                     )}
-                      {sanitizedConfig.publications.length !== 0 && (
-                      <PublicationCard
-                        loading={loading}
-                        publications={sanitizedConfig.publications}
-                      />
-                    )}
+                   
                  
                     {sanitizedConfig.projects.external.projects.length !==
                       0 && (
